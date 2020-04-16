f0adb9a24b64cabd97690847e8dc8f78ce18dbc4
Daniel Olshansky <olshansky.daniel@gmail.com>
2020-04-16 09:10:17 -0700

    Overloading the Insertion Operator for Printing

--- OverloadingtheInsertionOperatorforPrinting/src/Overloading the Insertion Operator for Printing.cpp
+++ OverloadingtheInsertionOperatorforPrinting/src/Overloading the Insertion Operator for Printing.cpp
@@ -1,53 +1,107 @@
-//============================================================================
-// Name        : Overloading.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
-#include <ostream>
-#include <string>
+
 using namespace std;
 
-class Test {
+class Test{
 private:
-	int id;
-	string name;
+    int age;
+    string name;
 
 public:
-	Test() :
-			id(0), name("") {
-	}
+    Test(): age(0), name("") {
 
-	Test(int id, string name) :
-			id(id), name(name) {
+    }
 
-	}
+    Test(int age, string name): age(age), name(name) {
 
-	const Test &operator=(const Test &other) {
-		id = other.id;
-		name = other.name;
+    }
+    Test(const Test& other) {
+        *this = other;
+    }
 
-		return *this;
-	}
 
-	Test(const Test &other) {
-		*this = other;
-	}
+    const Test& operator = (const Test& other) {
+        age = other.age;
+        name = other.name;
+        return *this;
+    }
 
-	friend ostream &operator<<(ostream &out, const Test &test) {
-		out << test.id << ": " << test.name;
-		return out;
-	}
+    friend ostream& operator << (ostream& out, const Test& test);
+
 };
 
-int main() {
+ostream& operator << (ostream &out, const Test &test) {
+    out << test.name << ": " << test.age;
+    return out;
 
-	Test test1(10, "Mike");
-
-	cout << test1 << endl;
-
-	return 0;
 }
+
+int main() {
+    Test test1(1, "a");
+    Test test2(2, "b");
+
+    cout << test1 << " ; "<< test2 << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+////============================================================================
+//// Name        : Overloading.cpp
+//// Author      :
+//// Version     :
+//// Copyright   : Your copyright notice
+//// Description : Hello World in C++, Ansi-style
+////============================================================================
+//
+//#include <iostream>
+//#include <ostream>
+//#include <string>
+//using namespace std;
+//
+//class Test {
+//private:
+//	int id;
+//	string name;
+//
+//public:
+//	Test() :
+//			id(0), name("") {
+//	}
+//
+//	Test(int id, string name) :
+//			id(id), name(name) {
+//
+//	}
+//
+//	const Test &operator=(const Test &other) {
+//		id = other.id;
+//		name = other.name;
+//
+//		return *this;
+//	}
+//
+//	Test(const Test &other) {
+//		*this = other;
+//	}
+//
+//	friend ostream &operator<<(ostream &out, const Test &test) {
+//		out << test.id << ": " << test.name;
+//		return out;
+//	}
+//};
+//
+//int main() {
+//
+//	Test test1(10, "Mike");
+//
+//	cout << test1 << endl;
+//
+//	return 0;
+//}
