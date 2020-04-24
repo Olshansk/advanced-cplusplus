470bc719dd9b7b4c9398c09fddfd22dcbc335e7b
Daniel Olshansky <olshansky.daniel@gmail.com>
2020-04-24 08:44:31 -0700

    Function pointers

--- FunctionPointers/src/FunctionPointers.cpp
+++ FunctionPointers/src/FunctionPointers.cpp
@@ -1,27 +1,74 @@
-//============================================================================
-// Name        : Function.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 
 using namespace std;
 
-void test(int value) {
-	cout << "Hello: " << value << endl;
+void test() {
+    cout << "test" << endl;
 }
+
+void test1(int v1) {
+    cout << "test1 " << " v1 " << v1 << endl;
+}
+
+void test2(int v1, string v2) {
+    cout << "test2" << " v1: " << v1 << " v2: " << v2 << endl;
+}
+
+using FunctionTest2TypeV1 = void (*)(int, string);
+using FunctionTest2TypeV2 = std::add_pointer<void(int, string)>::type;
+typedef void (*FunctionTest2TypeV3)(int, string) ;
 
 int main() {
-	test(7);
+    test();
 
-	void (*pTest)(int) = test;
+    void (*pTest)() = test;
+    void (*pTestR)() = &test;
+    pTest();
+    pTestR();
 
-	pTest(8);
+    void (*pTest1)(int) = test1;
+    void (*pTest1R)(int) = &test1;
 
+    pTest1(10);
+    pTest1R(11);
 
+    FunctionTest2TypeV1 pTest2V1 = test2;
+    FunctionTest2TypeV2 pTest2V2 = test2;
+    FunctionTest2TypeV3 pTest2V3 = test2;
+    pTest2V1(1, "v1");
+    pTest2V2(2, "v2");
+    pTest2V3(3, "v3");
 
-	return 0;
+//    void (*pTest2)(int) = test2;
+//    void (*pTest2R)(int) = &test2;
+
+    return 0;
 }
+
+////============================================================================
+//// Name        : Function.cpp
+//// Author      :
+//// Version     :
+//// Copyright   : Your copyright notice
+//// Description : Hello World in C++, Ansi-style
+////============================================================================
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//void test(int value) {
+//	cout << "Hello: " << value << endl;
+//}
+//
+//int main() {
+//	test(7);
+//
+//	void (*pTest)(int) = test;
+//
+//	pTest(8);
+//
+//
+//
+//	return 0;
+//}
--- TemplateFunctions1/src/Template Functions 1.cpp
+++ TemplateFunctions1/src/Template Functions 1.cpp
@@ -1,25 +1,59 @@
-//============================================================================
-// Name        : Template.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
-#include <string>
-using namespace std;
 
-template<class T>
-void print(T n) {
-	cout << n << endl;
+class Test {
+private:
+    std::string name;
+    int age;
+
+public:
+    Test(std::string name, int age): name(name), age(age) {
+    }
+
+    friend std::ostream& operator << (std::ostream& out, const Test& test);
+};
+
+std::ostream& operator << (std::ostream& out, const Test& test) {
+    std::cout << test.name << ": " << test.age << std::endl;
+    return out;
+}
+
+template <class T>
+void print(T t) {
+    std::cout << t << std::endl;
 }
 
 int main() {
-	print<string>("Hello");
-	print<int>(5);
+    print(1);
+    print("a");
 
-	print("Hi there");
-
-	return 0;
+    Test t("a", 10);
+    print(t);
+//    print();
+    return 0;
 }
+
+////============================================================================
+//// Name        : Template.cpp
+//// Author      :
+//// Version     :
+//// Copyright   : Your copyright notice
+//// Description : Hello World in C++, Ansi-style
+////============================================================================
+//
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//template<class T>
+//void print(T n) {
+//	cout << n << endl;
+//}
+//
+//int main() {
+//	print<string>("Hello");
+//	print<int>(5);
+//
+//	print("Hi there");
+//
+//	return 0;
+//}
