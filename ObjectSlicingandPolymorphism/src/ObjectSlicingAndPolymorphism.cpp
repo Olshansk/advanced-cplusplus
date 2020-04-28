d1ca1173471fa57f7d2232d7f2d7cc4f6b15ed41
Daniel Olshansky <olshansky.daniel@gmail.com>
2020-04-28 09:51:19 -0700

    Object slicing and polymorphism

--- ObjectSlicingandPolymorphism/src/ObjectSlicingAndPolymorphism.cpp
+++ ObjectSlicingandPolymorphism/src/ObjectSlicingAndPolymorphism.cpp
@@ -1,60 +1,128 @@
-//============================================================================
-// Name        : Object.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
+
 using namespace std;
 
+
 class Parent {
-private:
-	int one;
+protected:
+    int v1;
 
 public:
-	Parent(): one(0) {
 
-	}
+    Parent(): v1(0) {
 
-	Parent(const Parent &other): one(0) {
+    }
 
-		one = other.one;
-		cout << "copy parent" << endl;
-	}
+    Parent(int v1): v1(v1) {
 
-	virtual void print() {
-		cout << "parent" << endl;
-	}
+    }
 
-	virtual ~Parent() {
+    Parent (const Parent& other) {
+        cout << "Parent copy constructor" << endl;
+        v1 = other.v1;
+    }
 
-	}
+//    virtual void print() {
+    virtual void print() {
+        cout << "Parent: " << v1 << endl;
+    }
 };
 
 class Child: public Parent {
 private:
-	int two;
+    int v2;
 
 public:
-	Child(): two(0) {
 
-	}
+    Child(): v2(0) {
+        v1 = 10;
+    }
 
-	void print() {
-		cout << "child" << endl;
-	}
+    void print() {
+        cout << "Child: v1: " << v1 << " v2: " << v2 << endl;
+    }
+
 };
 
 int main() {
+    Parent p1(1);
+    p1.print();
 
-	Child c1;
-	Parent &p1 = c1;
-	p1.print();
+    Child c;
+    Parent& p2 = c;
+    c.print();
+    p2.print();
 
-	Parent p2 = Child();
-	p2.print();
+    Parent p3 = c;
+    p3.print();
 
-	return 0;
+    Parent p4(c);
+    p4.print();
+
+
+//    Child
+//    return 0;
 }
+
+
+////============================================================================
+//// Name        : Object.cpp
+//// Author      :
+//// Version     :
+//// Copyright   : Your copyright notice
+//// Description : Hello World in C++, Ansi-style
+////============================================================================
+//
+//#include <iostream>
+//using namespace std;
+//
+//class Parent {
+//private:
+//	int one;
+//
+//public:
+//	Parent(): one(0) {
+//
+//	}
+//
+//	Parent(const Parent &other): one(0) {
+//
+//		one = other.one;
+//		cout << "copy parent" << endl;
+//	}
+//
+//	virtual void print() {
+//		cout << "parent" << endl;
+
+//	}
+//
+//	virtual ~Parent() {
+//
+//	}
+//};
+//
+//class Child: public Parent {
+//private:
+//	int two;
+//
+//public:
+//	Child(): two(0) {
+//
+//	}
+//
+//	void print() {
+//		cout << "child" << endl;
+//	}
+//};
+//
+//int main() {
+//
+//	Child c1;
+//	Parent &p1 = c1;
+//	p1.print();
+//
+//	Parent p2 = Child();
+//	p2.print();
+//
+//	return 0;
+//}
