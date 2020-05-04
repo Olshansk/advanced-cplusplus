e12fd35758b873683ffaf18376bf0a0a0193ff92
Daniel Olshansky <olshansky.daniel@gmail.com>
2020-05-04 08:54:23 -0700

    Abstract classes

--- AbstractClasses/src/AbstractClasses.cpp
+++ AbstractClasses/src/AbstractClasses.cpp
@@ -1,69 +1,124 @@
-//============================================================================
-// Name        : Abstract.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 class Animal {
 public:
-	virtual void run()=0;
-	virtual void speak() = 0;
+    virtual void run() const =0;
+    virtual void voice() const =0;
 };
 
 class Dog: public Animal {
 public:
-	virtual void speak() {
-		cout << "Woof!" << endl;
-	}
+    virtual void run() const {
+        cout << "Dog is running" << endl;
+    }
 };
 
-class Labrador: public Dog {
+class Lab: public Dog {
 public:
-	Labrador() {
-		cout << "new labrador" << endl;
-	}
-	virtual void run() {
-		cout << "Labrador running" << endl;
-	}
+    virtual void voice() const {
+        cout << "Lab is speaking" << endl;
+    }
 };
 
-class Husky: public Dog
-{
-public:
-	Husky()
-	{
-		cout << "new husky" << endl;
-	}
-	virtual void run()
-	{
-		cout << "Husky running" << endl;
-	}
-};
-
-void test(Animal &a) {
-	a.run();
+void animalRunsAndSpeak(const Animal& a) {
+    a.voice();
+    a.run();
+    cout << endl;
 }
 
 int main() {
 
-	Labrador lab;
-	lab.run();
-	lab.speak();
+    Lab labs[5];
+    for (const Lab& l : labs) {
+        animalRunsAndSpeak(l);
+    }
 
-	Animal *animals[5];
-	animals[0] = &lab;
+    vector<Lab> labs2(5);
+    for (const Lab& l : labs2) {
+        animalRunsAndSpeak(l);
+    }
 
-	animals[0]->speak();
+    cout << labs2.size() << endl;
+    cout << labs2.capacity() << endl;
 
-	Husky husky;
+    // Can't instantiate abstract class here
+//    Dog dogs[5];
+//    for (const Dog& d : dogs) {
+//        animalRunsAndSpeak(d);
+//    }
 
-	test(lab);
-	test(husky);
-
-	return 0;
+    return 0;
 }
+
+////============================================================================
+//// Name        : Abstract.cpp
+//// Author      :
+//// Version     :
+//// Copyright   : Your copyright notice
+//// Description : Hello World in C++, Ansi-style
+////============================================================================
+//
+//#include <iostream>
+//using namespace std;
+//
+//class Animal {
+//public:
+//	virtual void run()=0;
+//	virtual void speak() = 0;
+//};
+//
+//class Dog: public Animal {
+//public:
+//	virtual void speak() {
+//		cout << "Woof!" << endl;
+//	}
+//};
+//
+//class Labrador: public Dog {
+//public:
+//	Labrador() {
+//		cout << "new labrador" << endl;
+//	}
+//	virtual void run() {
+//		cout << "Labrador running" << endl;
+//	}
+//};
+//
+//class Husky: public Dog
+//{
+//public:
+//	Husky()
+//	{
+//		cout << "new husky" << endl;
+//	}
+//	virtual void run()
+//	{
+//		cout << "Husky running" << endl;
+//	}
+//};
+//
+//void test(Animal &a) {
+//	a.run();
+//}
+//
+//int main() {
+//
+//	Labrador lab;
+//	lab.run();
+//	lab.speak();
+//
+//	Animal *animals[5];
+//	animals[0] = &lab;
+//
+//	animals[0]->speak();
+//
+//	Husky husky;
+//
+//	test(lab);
+//	test(husky);
+//
+//	return 0;
+//}
