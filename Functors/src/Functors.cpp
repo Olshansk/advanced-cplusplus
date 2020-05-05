54842c84ff2e937e687c7fab0998ec74a538a511
Daniel Olshansky <olshansky.daniel@gmail.com>
2020-05-05 09:57:01 -0700

    Functors

--- Functors/src/Functors.cpp
+++ Functors/src/Functors.cpp
@@ -1,57 +1,104 @@
-//============================================================================
-// Name        : Functors.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Functors in C++.
-//============================================================================
-
 #include <iostream>
 
 using namespace std;
 
-struct Test
-{
-	virtual bool operator()(string &text) = 0;
+struct Test {
+    virtual bool operator()(string& test)=0;
 };
 
-struct MatchTest : Test 
-{
-	virtual bool operator()(string &text){
-		return text == "lion";
-	}
+struct MatchTest : public Test {
+    virtual bool operator()(string& test) {
+        return test == "valid" ? true : false;
+    }
 };
 
-struct MyMatchTest : Test
-{
-	virtual bool operator()(string &text) {
-		return text == "text";
-	}
-};
-
-void check(string text, Test &t)
-{
-	if(t(text))
-	{
-		cout << "Text matches!" << endl;
-	}
-	else
-	{
-		cout << "No match." << endl;
-	}
+void check_test(string& text, Test& tester) {
+    if (tester(text)) {
+        cout << "Tester matched string!" << endl;
+    } else {
+        cout << "Tester did not match string..." << endl;
+    }
 }
 
 int main() {
-	
-	MatchTest pred;
-	string value = "lion";
-	
-	cout << pred(value) << endl;
 
-	check(value, pred);
+    MatchTest m;
+    Test *t = &m;
 
-	MyMatchTest myPred;
-	check(value, myPred);
+    string valid = "valid";
+    string invalid = "invalid";
 
-	return 0;
+    cout << "About to test 'Matchtest'\n" << endl;
+    check_test(valid, m);
+    check_test(invalid, m);
+
+    cout << "About to test 'Test'\n" << endl;
+    check_test(valid, *t);
+    check_test(invalid, *t);
+
+    return 0;
 }
+
+
+
+
+
+
+
+////============================================================================
+//// Name        : Functors.cpp
+//// Author      :
+//// Version     :
+//// Copyright   : Your copyright notice
+//// Description : Functors in C++.
+////============================================================================
+//
+//#include <iostream>
+//
+//using namespace std;
+//
+//struct Test
+//{
+//	virtual bool operator()(string &text) = 0;
+//};
+//
+//struct MatchTest : Test
+//{
+//	virtual bool operator()(string &text){
+//		return text == "lion";
+//	}
+//};
+//
+//struct MyMatchTest : Test
+//{
+//	virtual bool operator()(string &text) {
+//		return text == "text";
+//	}
+//};
+//
+//void check(string text, Test &t)
+//{
+//	if(t(text))
+//	{
+//		cout << "Text matches!" << endl;
+//	}
+//	else
+//	{
+//		cout << "No match." << endl;
+//	}
+//}
+//
+//int main() {
+//
+//	MatchTest pred;
+//	string value = "lion";
+//
+//	cout << pred(value) << endl;
+//
+//	check(value, pred);
+//
+//	MyMatchTest myPred;
+//	check(value, myPred);
+//
+//	return 0;
+//}
