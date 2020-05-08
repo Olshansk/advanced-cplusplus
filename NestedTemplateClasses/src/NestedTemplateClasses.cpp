bfd1f92c44fe2ccabebd91c6a803a312ac7e9d9e
Daniel Olshansky <olshansky.daniel@gmail.com>
2020-05-08 09:03:26 -0700

    Nested template classes + ring implementation

--- NestedTemplateClasses/src/NestedTemplateClasses.cpp
+++ NestedTemplateClasses/src/NestedTemplateClasses.cpp
@@ -1,33 +1,55 @@
-//============================================================================
-// Name        : NestedTemplateClasses.cpp
-// Author      : John Purcell
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Nested Template classes
-//============================================================================
-
 #include <iostream>
-#include <string>
 #include "ring.h"
+
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
 
-	ring<string>::iterator it;
-	it.print();
+    ring<int> r(3);
+    r.add(1);
+    r.add(2);
+    r.add(3);
+    r.add(4);
 
-	ring<string> textRing(3);
-	
-	textRing.add("one");
-	textRing.add("two");
-	textRing.add("three");
-	textRing.add("four");
+    for (int i = 0; i < r.size(); i++) {
+        cout << r.get(i) << endl;
+    }
 
-
-	for(auto i =0; i < textRing.size(); i++)
-	{
-		cout << textRing.get(i) << endl;
-	}
-
-	return 0;
+    return 0;
 }
+
+////============================================================================
+//// Name        : NestedTemplateClasses.cpp
+
+
+//// Author      : John Purcell
+//// Version     :
+//// Copyright   : Your copyright notice
+//// Description : Nested Template classes
+////============================================================================
+//
+//#include <iostream>
+//#include <string>
+//#include "ring.h"
+//using namespace std;
+//
+//int main() {
+//
+//	ring<string>::iterator it;
+//	it.print();
+//
+//	ring<string> textRing(3);
+//
+//	textRing.add("one");
+//	textRing.add("two");
+//	textRing.add("three");
+//	textRing.add("four");
+//
+//
+//	for(auto i =0; i < textRing.size(); i++)
+//	{
+//		cout << textRing.get(i) << endl;
+//	}
+//
+//	return 0;
+//}
--- NestedTemplateClasses/src/ring.h
+++ NestedTemplateClasses/src/ring.h
@@ -1,66 +1,113 @@
-//ring.h
-
-#pragma once
+#ifndef RING_H_
+#define RING_H_
+#include <iostream>
 #include <vector>
 
-// Note that this implementation of the ring class was written by 
-// Paul T (DeveloperPaul123@github)
-template <typename T>
-class ring
-{
-	std::vector<T> _data;
-	int _cur_index;
+using namespace std;
+
+template <class T>
+class ring {
+private:
+    vector<T> _data;
+    int _cur_index = 0;
+
 public:
-	class iterator;
+    class iterator;
 
-	ring(int size) : _cur_index(0) {
-		_data.reserve(size);
-		for(auto i = 0; i < size; i++)
-		{
-			_data.push_back(T());
-		}
-	}
+    ring(int size) {
+//        _data.reserve(size);
+        _data.resize(size);
+    }
 
-	void add(T value)
-	{
-		if (_cur_index < _data.size())
-		{
-			_data[_cur_index] = value;
-			_cur_index++;
-		}
-		else
-		{
-			_cur_index = 0;
-			_data[_cur_index] = value;
-		}
+    void add(T t) {
+        _data[_cur_index] = t;
+        _cur_index = _cur_index == _data.size() - 1 ? 0 : _cur_index + 1;
+    }
 
-	}
+    T get(int i) {
+        return _data[i];
+    }
 
-	T get(int index)
-	{
-		if (index < _data.size())
-		{
-			return _data[index];
-		}
-		return T();
-	}
+    int size() {
+        return _data.size();
+    }
 
-	int size()
-	{
-		return static_cast<int>(_data.size());
-	}
-	
 };
 
-template<typename T>
-class ring<T>::iterator
-{
-public:
-	void print() const;
+template <class T>
+class ring<T>::iterator {
+
 };
 
-template<typename T>
-void ring<T>::iterator::print() const
-{
-	std::cout << "Hello from iterator." << std::endl;
-}
+# endif
+
+
+
+
+
+////ring.h
+//
+//#pragma once
+//#include <vector>
+//
+//// Note that this implementation of the ring class was written by
+//// Paul T (DeveloperPaul123@github)
+//template <typename T>
+//class ring
+//{
+//	std::vector<T> _data;
+//	int _cur_index;
+//public:
+//	class iterator;
+//
+//	ring(int size) : _cur_index(0) {
+//		_data.reserve(size);
+//		for(auto i = 0; i < size; i++)
+//		{
+//			_data.push_back(T());
+//		}
+//	}
+//
+//	void add(T value)
+//	{
+//		if (_cur_index < _data.size())
+//		{
+//			_data[_cur_index] = value;
+//			_cur_index++;
+//		}
+//		else
+//		{
+//			_cur_index = 0;
+//			_data[_cur_index] = value;
+//		}
+//
+//	}
+//
+//	T get(int index)
+//	{
+//		if (index < _data.size())
+//		{
+//			return _data[index];
+//		}
+//		return T();
+//	}
+//
+//	int size()
+//	{
+//		return static_cast<int>(_data.size());
+//	}
+//
+//};
+//
+//template<typename T>
+//class ring<T>::iterator
+//{
+//public:
+//	void print() const;
+//};
+//
+//template<typename T>
+//void ring<T>::iterator::print() const
+//{
+//	std::cout << "Hello from iterator." << std::endl;
+//}
