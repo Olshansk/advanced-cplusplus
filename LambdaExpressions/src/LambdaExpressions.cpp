1f2435c582105c88d72a3395037f64f08d00c651
Daniel Olshansky <olshansky.daniel@gmail.com>
2020-05-26 08:22:34 -0700

    Simple lambda expression

--- LambdaExpressions/src/LambdaExpressions.cpp
+++ LambdaExpressions/src/LambdaExpressions.cpp
@@ -1,31 +1,50 @@
-//============================================================================
-// Name        : Elision.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 
 using namespace std;
 
-void test(void (*pFunc)()) 
-{
-	pFunc();
+void test(void (*pFunc)()) {
+  pFunc();
 }
 
-int main() {
+int main(int argc, char* argv[]) {
 
-	auto func = []()
-	{
-		cout << "Hello" << endl;
-	};
+  auto func = []() {
+    cout << "Test" << endl;
+  };
 
-	func();
+  test(func);
 
-	test(func);
-
-	test([]() {cout << "Hello again" << endl; });
-	return 0;
+  return 0;
 }
+
+// //============================================================================
+// // Name        : Elision.cpp
+// // Author      :
+// // Version     :
+// // Copyright   : Your copyright notice
+// // Description : Hello World in C++, Ansi-style
+// //============================================================================
+//
+// #include <iostream>
+//
+// using namespace std;
+//
+// void test(void (*pFunc)())
+// {
+// 	pFunc();
+// }
+//
+// int main() {
+//
+// 	auto func = []()
+// 	{
+// 		cout << "Hello" << endl;
+// 	};
+//
+// 	func();
+//
+// 	test(func);
+//
+// 	test([]() {cout << "Hello again" << endl; });
+// 	return 0;
+// }
